daf94ef6b880059729f5bd7b16fdca07ae4fbf38
Chaoran Chen <mail@chaoran-chen.de>
2023-01-03 14:14:35 +0100

    Add Bullet and Thumbnail components

---
+++ src/lib/Bullet.svelte
@@ -0,0 +1,17 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+
+  export let igBulletClass: string;
+  export let currentIndex: number;
+  export let index: number;
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<button
+  type="button"
+  class={igBulletClass}
+  on:click={(event) => dispatch('click', event)}
+  aria-pressed={currentIndex === index}
+  aria-label={`Go to Slide ${index + 1}`}
+/>
---
+++ src/lib/Thumbnail.svelte
@@ -0,0 +1,42 @@
+<script lang="ts">
+  import { createEventDispatcher } from 'svelte';
+  import type { TItem } from '$lib/types';
+
+  export let index: number;
+  export let currentIndex: number;
+  export let igThumbnailClass: string;
+  export let slideOnThumbnailOver: boolean;
+  export let item: TItem;
+
+  const dispatch = createEventDispatcher();
+</script>
+
+<button
+  type="button"
+  tabIndex="0"
+  aria-pressed={currentIndex === index}
+  aria-label={`Go to Slide ${index + 1}`}
+  class={igThumbnailClass}
+  on:mouseleave={slideOnThumbnailOver ? (event) => dispatch('mouseleave', event) : null}
+  on:mouseover={(event) => dispatch('mouseover', event)}
+  on:focus={(event) => dispatch('focus', event)}
+  on:keyup={(event) => dispatch('keyup', event)}
+  on:click={(event) => dispatch('click', event)}
+>
+  <span class="image-gallery-thumbnail-inner">
+    <img
+      class="image-gallery-thumbnail-image"
+      src={item.thumbnail}
+      height={item.thumbnailHeight}
+      width={item.thumbnailWidth}
+      alt={item.thumbnailAlt}
+      title={item.thumbnailTitle}
+      loading={item.thumbnailLoading}
+    />
+    {#if item.thumbnailLabel}
+      <div class="image-gallery-thumbnail-label">
+        {item.thumbnailLabel}
+      </div>
+    {/if}
+  </span>
+</button>
